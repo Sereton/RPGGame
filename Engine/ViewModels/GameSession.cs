60f8ab455a6848dc9296a9998dc65796613b4ab2
Jay Hernandez <ajuan_jose@hotmail.com>
2020-06-05 04:18:40 -0600

    Added Scene Navigation Panel

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -11,6 +11,7 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation {get; set; }
 
         public GameSession()
         {
@@ -19,8 +20,17 @@ namespace Engine.ViewModels
             CurrentPlayer.CharacterClass = "Fighter";
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.Gold = 1000000;
-            CurrentPlayer.ExperiencePoints = 10;
+            CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Description = "This is your house fam";
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.ImageName = "/Engine;component/Images/Home.png";
+
+
         }
 
     } 
